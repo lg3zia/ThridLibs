36914a4c1369959eac6a41be9963aacfe4e5e51b
lg3zi <gang.luo@bridgelec.com>
2016-10-26 10:27:04 +0800

    LruCacheAty:add this class

--- app/src/main/java/lg/com/thirdlibstraining/DiskLruCacheAty.java
+++ app/src/main/java/lg/com/thirdlibstraining/DiskLruCacheAty.java
@@ -1,16 +1,44 @@
 package lg.com.thirdlibstraining;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.util.LruCache;
+import android.widget.ImageView;
 
 public class DiskLruCacheAty extends BaseActivity {
+
+    LruCache<String, Bitmap> mMemoryCache;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disk_lru_cache_aty);
+        initView();
+        initData();
+
     }
 
+    private void initView() {
+        imageView = (ImageView) findViewById(R.id.iv1);
+    }
 
+    private void initData() {
+        int memoryCacheSize = (int) (Runtime.getRuntime().maxMemory() / 8);
+        mMemoryCache = new LruCache<String, Bitmap>(memoryCacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.boy);
+        mMemoryCache.put("iv_boy", bitmap);
+
+        imageView.setImageBitmap(mMemoryCache.get("iv_boy"));
+
+    }
 
 
 }
--- app/src/main/java/lg/com/thirdlibstraining/MainActivity.java
+++ app/src/main/java/lg/com/thirdlibstraining/MainActivity.java
@@ -9,15 +9,20 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_disk_lru_cache).setOnClickListener(this);
+        this.findViewById(R.id.btn_disk_lru_cache).setOnClickListener(this);
+        this.findViewById(R.id.btn_lru_cache).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.btn_lru_cache:
+                startAty(LruCacheAty.class);
+                break;
             case R.id.btn_disk_lru_cache:
                 startAty(DiskLruCacheAty.class);
                 break;
         }
     }
+
 }
